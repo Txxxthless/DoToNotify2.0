5b28cccb17998565579c1ada7863d2f0f9d5967c
Txxxthless <lepash85@gmail.com>
2023-03-01 18:09:07 +0200

    Created a basis UI.

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using DoToNotify2._0.Core;
+using System;
+
+namespace DoToNotify2._0.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
